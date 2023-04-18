dfceafddc15e390f8cf79d819b25916e45e7a74f
DenStrateg <106665225+DenStrateg@users.noreply.github.com>
2023-04-19 01:44:07 +0300

    Ticket Class (.cpp)

---
+++ KCTT/KCTT/Ticket.cpp
@@ -0,0 +1,26 @@
+#include "Ticket.h"
+#include "Event.h"
+
+Ticket::Ticket(std::string row, std::string place, bool isBooked)
+{
+}
+
+std::string Ticket::getCustomer()
+{
+}
+
+std::string Ticket::setCustomer()
+{
+}
+
+std::string Ticket::getEvent()
+{
+}
+
+std::string Ticket::setEvent()
+{
+}
+
+std::string Ticket::getId()
+{
+}
