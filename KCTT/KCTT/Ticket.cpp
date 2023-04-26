9c92e8098a5d261ca4580acb803c3b90aa74ea21
DenStrateg <106665225+DenStrateg@users.noreply.github.com>
2023-04-26 13:39:24 +0300

    Ticket (Logic + ShowTicket)

--- KCTT/KCTT/Ticket.cpp
+++ KCTT/KCTT/Ticket.cpp
@@ -3,27 +3,39 @@
 
 Ticket::Ticket(std::string row, std::string place, bool isBooked)
 {
+	this->row = row;
+	this->place = place;
+	this->isBooked = isBooked;
 }
 
-string Ticket::getCustomer()
+string Ticket::getCustomerId()
 {
-	return this->Customer;
+	return this->Customer_Id;
 }
 
-void Ticket::setCustomer()
+void Ticket::setCustomerId(std::string newCustomer)
 {
+	this->Customer_Id = newCustomer;
 }
 
-string Ticket::getEvent()
+string Ticket::getEventId()
 {
-	return this->Event;
+	return this->Event_Id;
 }
 
-void Ticket::setEvent()
+void Ticket::setEventId(std::string newEvent)
 {
+	this->Event_Id = newEvent;
 }
 
 string Ticket::getId()
 {
 	return this->id;
+}
+
+void Ticket::ShowTicket()
+{
+	cout << "Ticket_Id\t" << this->id << endl;
+	cout << "Event\t" << this->Event_Id << endl;
+	cout << "Customer\t" << this->Customer_Id << endl;
 }
--- KCTT/KCTT/Ticket.h
+++ KCTT/KCTT/Ticket.h
@@ -6,14 +6,17 @@
 class Ticket
 {
 private:
+	std::string row;
+	std::string place;
 	std::string id;
-	std::string Event;
-	std::string Customer;
+	std::string Event_Id;
+	std::string Customer_Id;
 public:
 	Ticket(std::string row, std::string place, bool isBooked);
-	string getCustomer();
-	void setCustomer();
-	string getEvent();
-	void setEvent();
+	string getCustomerId();
+	void setCustomerId(std::string newCustomer);
+	string getEventId();
+	void setEventId(std::string newEvent);
 	string getId();
+	void ShowTicket();
 };
