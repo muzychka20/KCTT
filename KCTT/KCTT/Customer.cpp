9a0fb92752c3975484df79edf2d71a44dd9f5832
Olga Kostrenko <olgakostrenko1@gmail.com>
2023-04-26 13:28:25 +0300

    Menu

--- KCTT/KCTT/Customer.cpp
+++ KCTT/KCTT/Customer.cpp
@@ -1,6 +1,11 @@
 #include "Customer.h"
 #include "TicketStore.h"
 #include "EventStore.h"
+#include"Event.h"
+#include"Ticket.h"
+#include <iostream>
+#include <stdio.h>
+
 
 Customer::Customer(std::string name, std::string login, std::string password) : User(name, login, password)
 {
@@ -8,11 +13,142 @@ Customer::Customer(std::string name, std::string login, std::string password) : 
 
 void Customer::PrintMenu(char *action)
 {
+    std::cout << "\t Welcome customer!" << endl;
+    std::cout << "      Please select an action:" << endl;
+    std::cout << "  1. Buy a ticket" << endl;
+    std::cout << "  2. Find an event by name" << endl;
+    std::cout << "  3. Find an event by date" << endl;
+    std::cout << "  4. View all events " << endl;
+    std::cout << "  5. View purchased tickets" << endl;
+    std::cout << "  6. Cancel a ticket" << endl;
+    std::cout << "  Enter your choice (1-6): ";
+    std::cin >> *action;
 
 }
 
 void Customer::ActivateMenu(char *action)
 {
+    string eventName, eventDate, row, place, id;
+    bool isBooked;
+    int choose = 0;
+    system("cls");
+    switch (*action)
+    {
+    case 1: {
+        ShowAllEvents();
+        cout << "Which event would you like to attend. Enter the name of the event: ";
+        cin >> eventName;
+        FindEventByName(eventName);
+    
+
+        bool eventExists = false;
+        for (size_t i = 0; i < EventStore::events.size(); ++i)
+        {
+            if (EventStore::events[i].name == eventName) {
+                eventExists = true;
+                break;
+            }
+        }
+
+        if (eventExists) {
+
+            cout << "The event exists.Do you want to buy a ticket?(1/0): ";
+            cin >> choose;
+            if (choose == 1) {
+                int numTickets = 0;
+                cout << "Choose the row: ";
+                cin >> row;
+                cout << "Choose the place: ";
+                cin >> place;
+
+                //тут должна быть проверка на то забронирован ли билет
+
+                cout << "How many tickets do you want to buy? ";
+                cin >> numTickets;
+                for (int i = 0; i < TicketStore::tickets.size(); ++i) {
+                    Ticket ticket(row, place, eventExists);
+                    AddTicket(ticket);
+                }
+                
+            };
+            if (choose == 0) cout << "You don't want to buy a ticket.";
+        }
+        else {
+            cout << "The event does not exist."<<endl;
+
+        }
+        break;
+        
+    }
+    case 2: {  
+        cout << "Which event would you like to find. Enter the name of the event: ";
+        cin >> eventName;
+        FindEventByName(eventName);
+
+        bool eventExists = false;
+        for (size_t i = 0; i < EventStore::events.size(); i++)
+        {
+            if (EventStore::events[i].name == eventName) {
+                eventExists = true;
+                break;
+            }
+        }
+        if (eventExists) {
+            cout << "The event exists." << endl;
+        }
+        else {
+            cout << "The event does not exist." << endl;
+        }
+    }
+    case 3: {
+        cout << "When would you like to attend event. Enter the date of the event: ";
+        cin >> eventDate;
+        FindEventByName(eventDate);
+
+        bool eventExists = false;
+        for (size_t i = 0; i < EventStore::events.size(); i++)
+        {
+            if (EventStore::events[i].date == eventDate) {
+                eventExists = true;
+                break;
+            }
+        }
+        if (eventExists) {
+            cout << "At this day the event exists." << endl;
+        }
+        else {
+            cout << "The event does not exist at this day." << endl;
+        }
+
+    }
+    case 4: {
+        ShowAllEvents();
+    }
+    case 5: {
+        if (TicketStore::tickets.size() == 0) {
+            cout << "No tickets have been ordered yet." << endl;
+            return;
+        }
+        cout << "The following tickets have been ordered:" << endl;
+        for (size_t i = 0; i < TicketStore::tickets.size(); i++) {
+            cout << "Ticket " << i + 1 << ":" << endl;
+            ShowTickets();
+            cout << endl;
+        }
+    }
+    case 6: {
+        cout << "Enter id: ";
+        cin >> id;
+        DeleteTicket(id);
+        break;
+    }
+    default:
+        std::cout << "Action not found." << std::endl;
+        std::cout << "Enter to continue..." << std::endl;
+        std::cin.ignore(10, '\n');
+        std::cin.get();
+        break;
+    }
 }
 
 void Customer::ShowTickets()
