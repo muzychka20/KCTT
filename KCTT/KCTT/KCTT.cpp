8d6bd231d16d01a890c830dacfe57ecb4f4d5a12
Dmitry Tavern <dogger.work@gmail.com>
2023-04-26 14:50:57 +0300

    feat: add logout

--- KCTT/KCTT/KCTT.cpp
+++ KCTT/KCTT/KCTT.cpp
@@ -7,9 +7,6 @@
 using namespace std;
 
 int main() {
-  User *user = NULL;
-  char user_action;
-
   // Setup admins
   UserStore::users.push_back(new Admin("Kyrylo Muzychka", "kyrylo", "1"));
   UserStore::users.push_back(new Admin("Dmitry Tavern", "dmitry", "2"));
@@ -22,37 +19,59 @@ int main() {
   UserStore::users.push_back(new Customer("Customer #2", "custom2", "1"));
   UserStore::users.push_back(new Customer("Customer #3", "custom3", "1"));
 
-  // Authorize the user
   do {
-    std::string username;
-    std::cout << "Enter login: ";
-    std::cin >> username;
+    User *user = NULL;
+    char user_action;
 
-    std::string password;
-    std::cout << "Enter password: ";
-    std::cin >> password;
+    system("cls");
 
-    for (size_t i = 0; i < UserStore::users.size(); i++) {
-      if (UserStore::users[i]->GetLogin() == username) {
-        if (UserStore::users[i]->GetPassword() == password) {
-          user = UserStore::users[i];
-          break;
+    std::cout << "Start menu" << std::endl;
+    std::cout << "1 - login" << std::endl;
+    std::cout << "2 - exit" << std::endl;
+    std::cin >> user_action;
+
+    system("cls");
+
+    if (user_action == '1')
+    {
+      // Authorize the user
+      do {
+        std::string username;
+        std::cout << "Enter login: ";
+        std::cin >> username;
+
+        std::string password;
+        std::cout << "Enter password: ";
+        std::cin >> password;
+
+        for (size_t i = 0; i < UserStore::users.size(); i++) {
+          if (UserStore::users[i]->GetLogin() == username) {
+            if (UserStore::users[i]->GetPassword() == password) {
+              user = UserStore::users[i];
+              break;
+            }
+          }
         }
-      }
+
+        if (user == NULL) {
+          std::cout << "Login or password is invalid." << std::endl;
+        }
+      } while (user == NULL);
+
+      // Call the user menu
+      do {
+        system("cls");
+        user->PrintMenu(&user_action);
+        system("cls");
+        user->ActivateMenu(&user_action);
+        if (user_action == 'e')
+          break;;
+      } while (true);
     }
 
-    if (user == NULL) {
-      std::cout << "Login or password is invalid." << std::endl;
+    if (user_action == '2')
+    {
+      exit(0);
     }
-  } while (user == NULL);
-
-  // Call the user menu
-  do {
-    system("cls");
-    user->PrintMenu(&user_action);
-    system("cls");
-    user->ActivateMenu(&user_action);
-    if (user_action == 'e')
-      break;
   } while (true);
 }
