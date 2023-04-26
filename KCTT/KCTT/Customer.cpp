2de63dd7c438a61db016b8079d6deafc659d631c
Kyrylo Muzychka <kyryll2016@gmail.com>
2023-04-26 13:59:55 +0300

    Customer ShowTicket()

--- KCTT/KCTT/Customer.cpp
+++ KCTT/KCTT/Customer.cpp
@@ -155,7 +155,7 @@ void Customer::ActivateMenu(char *action)
 void Customer::ShowTickets()
 {
     for (size_t i = 0; i < boughtTickets.size(); i++) {
-        cout << boughtTickets[i].ShowTicket() << endl;
+        boughtTickets[i].ShowTicket();
         cout << "----------" << endl;
     }
 }
