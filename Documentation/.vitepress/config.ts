62cbd3bc0e07d01b3a24638e34eab0ffd586ccbe
Dmitry Tavern <dogger.work@gmail.com>
2023-04-27 14:17:51 +0300

    docs: add vitepress

---
+++ Documentation/.vitepress/config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vitepress";
+
+export const nav = [
+  { text: "Home", link: "/" },
+  { text: "Examples", link: "/markdown-examples" },
+];
+
+export const sidebar = [
+  {
+    text: "Examples",
+    items: [
+      { text: "Markdown Examples", link: "/markdown-examples" },
+      { text: "Runtime API Examples", link: "/api-examples" },
+    ],
+  },
+];
+
+export default defineConfig({
+  title: "Keep Calm & Take Tickets",
+  description: "The university's OOP project is designed to gain experience in teamwork.",
+  themeConfig: {
+    nav,
+    sidebar,
+    socialLinks: [{ icon: "github", link: "https://github.com/KyryloMuzychka/KCTT" }],
+  },
+});
