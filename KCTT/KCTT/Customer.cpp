40c305f3d50d9f8394f8aa65d27536e6167c9020
Olga Kostrenko <olgakostrenko1@gmail.com>
2023-04-27 13:25:53 +0300

    Changes

---
+++ KCTT/KCTT/Customer.cpp
@@ -0,0 +1,205 @@
+#include "Customer.h"
+#include "TicketStore.h"
+#include "EventStore.h"
+#include"Event.h"
+#include"Ticket.h"
+#include <iostream>
+#include <stdio.h>
+
+
+Customer::Customer(std::string name, std::string login, std::string password) : User(name, login, password)
+{
+    this->role = "Customer";
+}
+
+void Customer::PrintMenu(char *action)
+{
+    std::cout << "\t Welcome customer!" << endl;
+    std::cout << "      Please select an action:" << endl;
+    std::cout << "  1. Buy a ticket" << endl;
+    std::cout << "  2. Find an event by name" << endl;
+    std::cout << "  3. Find an event by date" << endl;
+    std::cout << "  4. View all events " << endl;
+    std::cout << "  5. View purchased tickets" << endl;
+    std::cout << "  6. Cancel a ticket" << endl;
+    std::cout << "  Enter your choice (1-6): ";
+    std::cin >> *action;
+
+}
+
+void Customer::ActivateMenu(char *action)
+{
+    string eventName, eventDate, row, place, id;
+    bool isBooked;
+    int choose = 0;
+    system("cls");
+    switch (*action)
+    {
+    case '1': {
+        ShowAllEvents();
+        cout << "Which event would you like to attend. Enter the name of the event: ";
+        cin >> eventName;
+    
+
+        bool eventExists = false;
+        for (size_t i = 0; i < EventStore::events.size(); ++i)
+        {
+            if (EventStore::events[i].name == eventName) {
+                EventStore::events[i].ShowEvent();
+                eventExists = true;
+                break;
+            }
+        }
+
+        if (eventExists) {
+
+            cout << "The event exists.Do you want to buy a ticket?(1/0): ";
+            cin >> choose;
+            if (choose == 1) {
+              
+                cout << "Choose the row: ";
+                cin >> row;
+                cout << "Choose the place: ";
+                cin >> place;
+
+                for (int i = 0; i < TicketStore::tickets.size(); ++i)
+                {
+                    if (TicketStore::tickets[i].getRow() == row && TicketStore::tickets[i].getPlace() == place)
+                    {
+                        TicketStore::tickets[i].Booking();
+                        boughtTickets.push_back(TicketStore::tickets[i]);
+                        break;
+                    }
+                }             
+                
+            };
+            if (choose == 0) cout << "You don't want to buy a ticket.";
+        }
+        else {
+            cout << "The event does not exist."<<endl;
+
+        }
+        break;
+        
+    }
+    case '2': {  
+        cout << "Which event would you like to find. Enter the name of the event: ";
+        cin >> eventName;
+        
+
+        bool eventExists = false;
+        for (size_t i = 0; i < EventStore::events.size(); i++)
+        {
+            if (EventStore::events[i].name == eventName) 
+            {
+                FindEventByName(eventName);
+                eventExists = true;
+                break;
+            }
+        }
+        if (!eventExists)
+        {
+
+            cout << "The event does not exist." << endl;
+        }
+        break;
+    }
+    case '3': {
+        cout << "When would you like to attend event. Enter the date of the event: ";
+        cin >> eventDate;
+
+        bool eventExists = false;
+        for (size_t i = 0; i < EventStore::events.size(); i++)
+        {
+            if (EventStore::events[i].date == eventDate) {
+                FindEventByDate(eventDate);
+                eventExists = true;
+                break;
+            }
+        }
+        if (eventExists) {
+        
+            cout << "The event does not exist at this day." << endl;
+        }
+        break;
+    }
+    case '4': {
+        ShowAllEvents();
+        break;
+    }
+    case '5': {
+        if (boughtTickets.empty()) {
+            cout << "No tickets have been ordered yet." << endl;
+        }
+        else {
+        cout << "The following tickets have been ordered:" << endl;
+        ShowTickets(); 
+        }
+               
+        break;
+    }
+    case '6': {
+        cout << "Enter id: ";
+        cin >> id;
+        DeleteTicket(id);
+        break;
+    }
+    default:
+        std::cout << "Action not found." << std::endl;
+        std::cout << "Enter to continue..." << std::endl;
+        std::cin.ignore(10, '\n');
+        std::cin.get();
+        break;
+    }
+}
+
+void Customer::ShowTickets()
+{
+    for (size_t i = 0; i < boughtTickets.size(); i++) {
+        boughtTickets[i].ShowTicket();
+        cout << "----------" << endl;
+    }
+}
+
+void Customer::ShowAllEvents()
+{
+    for (size_t i = 0; i < EventStore::events.size(); i++) {
+        EventStore::events[i].ShowEvent();
+        cout << "----------" << endl;
+    }
+}
+
+void Customer::AddTicket(Ticket ticket)
+{
+	TicketStore::tickets.push_back(ticket);
+}
+
+void Customer::DeleteTicket(std::string ticketId)
+{
+    for (size_t i = 0; i < boughtTickets.size(); i++) {
+        if (boughtTickets[i].getId() == ticketId) {
+            boughtTickets.erase(boughtTickets.begin() + i);
+            break;
+        }
+    }
+}
+
+void Customer::FindEventByDate(std::string date)
+{
+    for (size_t i = 0; i < EventStore::events.size(); i++) {
+        if (EventStore::events[i].date == date) {
+            EventStore::events[i].ShowEvent();
+            break;
+        }
+    }
+}
+
+void Customer::FindEventByName(std::string name)
+{
+    for (size_t i = 0; i < EventStore::events.size(); i++) {
+        if (EventStore::events[i].name == name) {
+            EventStore::events[i].ShowEvent();
+            break;
+        }
+    }
+}
