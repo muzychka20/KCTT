d47619b31edcbfc74940fcc19a385274ca3b63f4
Podionboz <Podionboz@users.noreply.github.com>
2023-04-30 14:03:02 +0000

    deploy: 689f71d52a4d464eb2a38604427cd70b2d0567fe

---
+++ assets/api_users_user-admin.md.7808e4ee.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as a,o as t,O as o}from"./chunks/framework.3ec6c483.js";const p=JSON.parse('{"title":"user-admin class","description":"","frontmatter":{},"headers":[],"relativePath":"api/users/user-admin.md"}'),i={name:"api/users/user-admin.md"},d=o("",36),r=[d];function n(c,l,s,h,v,u){return t(),a("div",null,r)}const T=e(i,[["render",n]]);export{p as __pageData,T as default};
